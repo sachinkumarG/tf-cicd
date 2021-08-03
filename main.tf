f5717cea872f27de90e5171bd274a2a7917507db
sachinkumarG <sachin.gouri@gmail.com>
2021-08-03 22:49:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 provider "azurerm" {
     version         =   "~> 2.0"
+    alias           = "other_sub"
     client_id       =   var.client_id
     client_secret   =   var.client_secret
     subscription_id =   var.subscription_id
