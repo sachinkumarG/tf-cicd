963b3af65290d131c802d8ae49b8f17c7bac082f
sachinkumarG <sachin.gouri@gmail.com>
2021-08-03 22:45:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+provider "azurerm" {
+    version         =   "~> 2.0"
+    client_id       =   var.client_id
+    client_secret   =   var.client_secret
+    subscription_id =   var.subscription_id
+    tenant_id       =   var.tenant_id
+    
+    features {}
+}
+
 resource "azurerm_resource_group" "rg" {
   name = "test-mgmt-rg"
   location = "Southeast Asia"
